a00d4074ea60497302c6826db04e2a5c1c2582c7
RusselJay27 <ponferrada.russel27@gmail.com>
2020-08-29 19:14:50 +0800

    Modified wallet.

--- includes/api/v1/users/wallet/class-listing.php
+++ includes/api/v1/users/wallet/class-listing.php
@@ -9,8 +9,8 @@
      * @version 0.1.0
      * Here is where you add hook to WP to create our custom database if not found.
 	*/
-
 	class CP_Listing_Wallet {
+
         public static function listen(){
             return rest_ensure_response( 
                 self::listen_open()
@@ -18,23 +18,23 @@
         }
     
         public static function listen_open(){
+
             global $wpdb;
 
-            // // Step 1: Validate user
-            if ( DV_Verification::is_verified() == false ) {
-                return rest_ensure_response( 
-                    array(
-                        "status" => "unknown",
-                        "message" => "Please contact your administrator. Verification Issue!",
-                    )
-                );
-            }
-
+            // Step 1: Check if prerequisites plugin are missing
             $plugin = CP_Globals::verify_prerequisites();
             if ($plugin !== true) {
                 return array(
-                        "status" => "unknown",
-                        "message" => "Please contact your administrator. ".$plugin." plugin missing!",
+                    "status" => "unknown",
+                    "message" => "Please contact your administrator. ".$plugin." plugin missing!",
+                );
+            }
+
+            // Step 2: Validate user
+            if ( DV_Verification::is_verified() == false ) {
+                return array(
+                    "status" => "unknown",
+                    "message" => "Please contact your administrator. Verification issue!",
                 );
             }
             
@@ -42,14 +42,16 @@
             
             $type == '0'? $abbrev = NULL: $abbrev = $type;
             
+            // Step 3: Start mysql transaction
             $sql = "SELECT 
                 wall.hash_id,
                 wall.wpid,
                 cur.title,
                 cur.info
-                FROM 
-                    cp_wallets wall
-                INNER JOIN cp_currencies cur ON cur.ID = wall.currency WHERE wpid = %d
+            FROM 
+                cp_wallets wall
+            INNER JOIN 
+                cp_currencies cur ON cur.ID = wall.currency WHERE wpid = %d
             ";
 
             if (isset($_POST['type'])) {
@@ -58,8 +60,8 @@
                 }
             }
 
+            // Step 4: Return result
             $result =  $wpdb->get_results($wpdb->prepare($sql, $_POST['wpid']) );
-
             return array(
                 "status" => "success",
                 "data" => $result
--- includes/api/v1/users/wallet/class-select-balance.php
+++ includes/api/v1/users/wallet/class-select-balance.php
@@ -9,8 +9,8 @@
      * @version 0.1.0
      * Here is where you add hook to WP to create our custom database if not found.
 	*/
-
 	class CP_Select_Balance {
+
         public static function listen(){
             return rest_ensure_response( 
                 self::listen_open()
@@ -18,18 +18,10 @@
         }
     
         public static function listen_open(){
+
             global $wpdb;
-
-            // Step 1: Validate user
-            // if ( DV_Verification::is_verified() == false ) {
-            //     return rest_ensure_response( 
-            //         array(
-            //             "status" => "unknown",
-            //             "message" => "Please contact your administrator. Verification Issue!",
-            //         )
-            //     );
-            // }
-
+            
+            // Step 1: Check if prerequisites plugin are missing
             $plugin = CP_Globals::verify_prerequisites();
             if ($plugin !== true) {
                 return array(
@@ -38,6 +30,15 @@
                 );
             }
 
+            // Step 2: Validate user
+            if ( DV_Verification::is_verified() == false ) {
+                return array(
+                    "status" => "unknown",
+                    "message" => "Please contact your administrator. Verification issue!",
+                );
+            }
+
+            // Step 3: Check if required parameters are passed
             if (!isset($_POST['type'])) {
                 return array(
                     "status" => "unknown",
@@ -45,13 +46,15 @@
                 );
             }
 
+            // Step 4: Check if parameters passed are empty
             if (empty($_POST['type'])) {
                 return array(
-                    "status" => "unknown",
-                    "message" => "Please contact your administrator. Request unknown!",
+                    "status" => "failed",
+                    "message" => "Required fields cannot be empty.",
                 );
             }
             
+            // Step 5: Validate abbrevation length
             if (strlen($_POST['type']) != 3) {
                 return array(
                     "status" =>  "failed",
@@ -59,24 +62,26 @@
                 );
             }
 
+            // Step 6: Validate wallet and currency
             $get_key = $wpdb->get_row($wpdb->prepare("SELECT public_key, currency FROM cp_wallets WHERE wpid = %d  ", $_POST['wpid']));
 
             $get_currency = $wpdb->get_row($wpdb->prepare(" SELECT ID, title FROM cp_currencies WHERE abbrev = '%s'", $_POST['type']));
 
             if (!$get_currency) {
                 return array(
-                    "status" => "failed",
+                    "status" => "success",
                     "message" => "This currency does not exists.",
                 );
             }
 
             if ( $get_key->currency !==  $get_currency->ID   ) {
                 return array(
-                    "status" => "failed",
+                    "status" => "success",
                     "message" => "You dont have ".$get_currency->title." wallet.",
                 );
             }
 
+            // Step 7: Start mysql transaction
             $result = $wpdb->get_row(
                 $wpdb->prepare(" SELECT 
                     COALESCE(  
@@ -85,6 +90,7 @@
                         , 0 ) as balance 
                         FROM	cp_transaction", $get_key->public_key, $get_key->public_key));
 
+            // Step 8: Check if no rows found
             if (!$result) {
                 return array(
                     "status"  => "failed",
@@ -92,6 +98,7 @@
                 );
 
             }else{
+            // Step 9: Return result
                 return array(
                     "status"  => "success",
                     "data" => $result
